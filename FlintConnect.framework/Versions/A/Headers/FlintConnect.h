27310dc6c2601a717215da6c04828b9059c96faa
PC Nguyen <pc@flint.com>
2015-04-03 10:34:36 -0700

    initial project integration

---
+++ FlintConnect.framework/Versions/A/Headers/FlintConnect.h
@@ -0,0 +1,13 @@
+//
+//  FlintConnect.h
+//  FlintConnect
+//
+//  Created by PC on 4/3/15.
+//  Copyright (c) 2015 Flint. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FlintConnect : NSObject
+
+@end
