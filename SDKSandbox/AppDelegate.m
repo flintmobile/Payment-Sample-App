2951dda1bb1ebbb75e355aa5b901c76c83f8c1b7
PC Nguyen <pc@flint.com>
2015-09-03 15:01:52 -0700

    starting service

--- SDKSandbox/AppDelegate.m
+++ SDKSandbox/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <FlintConnectSDK/FlintConnectSDK.h>
 
 @interface AppDelegate ()
 
@@ -16,32 +17,32 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  // Override point for customization after application launch.
-  NSLog(@"%@", [FlintService sharedInstance].authToken);
 
+  [FlintAPIConfig sharedInstance].APIKey = @"4d72de8c70d3ba2e0fef18bd6231bdb7";
+  [FlintAPIConfig sharedInstance].environment = EnvironmentStaging;
+  [FlintAPIConfig sharedInstance].username = @"test0001@mailinator.com";
+  [FlintAPIConfig sharedInstance].password = @"T3st1ng1";
+  
+  [[FlintService sharedInstance] startServiceWithCompletion:^(FlintServiceStatus status, NSDictionary *userInfo) {
+    
+    NSError *error = [userInfo valueForKey:FlintServiceErrorKey];
+    if (!error) {
+      NSLog(@"==============SERVICE AUTHENTICATED AND START===================");
+      
+      FlintAccount *account = [userInfo valueForKey:FlintServiceAccountKey];
+      NSLog(@"Authenticated Account:%@", account.key);
+      
+      FlintMerchant *merchant = [userInfo valueForKey:FlintServiceMerchantKey];
+      NSLog(@"Merchant info:%@", [merchant businessName]);
+      
+      NSLog(@"====================END SERVICE INFO============================");
+    } else {
+      
+      NSLog(@"Flint Service Error:%@", error);
+    }
+  }];
+  
   return YES;
-}
-
-- (void)applicationWillResignActive:(UIApplication *)application {
-  // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-  // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-  // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-  // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-  // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-  // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-  // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
 @end
